b65828dabdeb6c1c6f1742b56fa2de29bbc2f070
shubh <shahshubh1010@gmail.com>
2022-03-17 16:01:08 +0530

    reload on action change

--- src/content/script.js
+++ src/content/script.js
@@ -85,7 +85,7 @@ ws.onmessage = (event) => {
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-	  if(key === 'isOn'){ // Whenever isOn key is changed reload the page
+	  if(key === 'isOn' || key === 'action'){ // Whenever isOn key is changed reload the page
 		  location.reload();
 	  }
   }
